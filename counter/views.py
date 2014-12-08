41c3a7282532a466713b2178c1e1cf42f882e066
shubhajeet2107 <dwij.stardust@gmail.com>
2014-12-09 00:50:39 +0530

    Added template view and show online count

--- counter/views.py
+++ counter/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from counter.models import Counter
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world !")
+	context = {'online_users': request.online.total}
+	return render(request, 'counter/index.html',context)
--- online/urls.py
+++ online/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'online.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^counter/', include('counter.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
