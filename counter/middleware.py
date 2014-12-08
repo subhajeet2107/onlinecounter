c7aecfb1ef41f113aa9f891503f2c0899e4c651f
shubhajeet2107 <dwij.stardust@gmail.com>
2014-12-09 00:26:56 +0530

    Added simple index view

--- counter/middleware.py
+++ counter/middleware.py
@@ -5,8 +5,7 @@ class CounterMiddleware(object):
     def process_request(self, request):
         now_time = datetime.now().time()
         online, create = Counter.objects.get_or_create(ip=request.META["REMOTE_ADDR"])
-        if not create:
-            online.visited_time = now_time
+        online.visited_time = now_time
         online.save()
         request.online = self
 
--- online/settings.py
+++ online/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'counter.middleware.CounterMiddleware',
 )
 
 ROOT_URLCONF = 'online.urls'
