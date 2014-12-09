59afef77c30aef0e22204ccf1bc0752884a98292
shubhajeet2107 <dwij.stardust@gmail.com>
2014-12-09 19:38:31 +0530

    Add route

--- counter/urls.py
+++ counter/urls.py
@@ -4,6 +4,7 @@ from counter import views
 
 urlpatterns = [
 	url(r'^get_visitor_count$', views.get_visitor_count, name='get_visitor_count'),
+	url(r'^end_session$', views.end_session, name='end_session'),
     url(r'^$', views.index, name='index'),
 
 ]
