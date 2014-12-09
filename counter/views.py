687255d8e75b07738c77907727a905c8051e46a5
shubhajeet2107 <dwij.stardust@gmail.com>
2014-12-09 20:27:47 +0530

    fix ip

--- counter/views.py
+++ counter/views.py
@@ -18,7 +18,7 @@ def get_visitor_count(request):
 def get_current_ip(request):
 	x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
 	if x_forwarded_for:
-		ip = x_forwarded_for.split(',')[0]
+		ip = x_forwarded_for.split(',')[-1]
 	else:
 		ip = request.META.get('REMOTE_ADDR')
 		return ip
