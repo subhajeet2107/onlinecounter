350a50a2f39650421e24ab76cff7a25e07e8f6df
shubhajeet2107 <dwij.stardust@gmail.com>
2014-12-09 20:03:22 +0530

    show ip

--- counter/urls.py
+++ counter/urls.py
@@ -5,6 +5,7 @@ from counter import views
 urlpatterns = [
 	url(r'^get_visitor_count$', views.get_visitor_count, name='get_visitor_count'),
 	url(r'^end_session$', views.end_session, name='end_session'),
+	url(r'^get_current_ip$', views.get_current_ip, name='get_current_ip'),
     url(r'^$', views.index, name='index'),
 
 ]
--- counter/views.py
+++ counter/views.py
@@ -14,6 +14,14 @@ def get_visitor_count(request):
 	total = Counter.objects.all().count()
 	return HttpResponse(total)
 
+def get_current_ip(request):
+	x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return HttpResponse(ip)
+    
 def end_session(request):
 	v_ip = request.POST.get('ip',False)
 	Counter.objects.filter(ip=v_ip).delete()
